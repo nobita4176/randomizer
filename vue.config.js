3f46bfba295e270cc6dbb4cf397c68b8fabfbc85
nobita <nobita4176@gmail.com>
2020-11-07 13:57:09 +0900

    icon

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,23 @@ module.exports = {
     sourceMap: true,
   },
   pwa: {
+    name: 'らんだまいざ',
+    themeColor: '#ca9ebb',
 	  iconPaths: {
       favicon32: 'favicon.ico',
       favicon16: null,
       appleTouchIcon: null,
       maskIcon: null,
       msTileImage: null,
+    },
+    manifestOptions: {
+      orientation: 'landscape',
+      icons: [
+        {
+          src: 'favicon.png',
+          sizes: '256x256',
+        },
+      ]
     },
   },
 };
