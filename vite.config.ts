dd76f8471a59357cfecc877548bc8cddf666f891
nobita4176 <nobita4176@gmail.com>
2022-03-25 00:53:47 +0900

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        lang: 'ja',
+        name: 'らんだまいざ',
+        background_color: '#ffffff',
+        theme_color: '#ca9ebb',
+        display: 'standalone',
+        orientation: 'landscape',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '256x256',
+          },
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
